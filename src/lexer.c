c77c7b2866032439a2ad48e7abc954059607e943
Rebecca Birgitta Anestad <anestad@linprog2.cs.fsu.edu>
2023-09-29 11:32:06 -0400

    adding code from 9/29 Please note it is a bit messy

--- src/lexer.c
+++ src/lexer.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 //NOT ALLOWED TO ADD ANYMORE #INCLUDE
+// #include <unistd.h> //can we include this? 
+#include <dirent.h>
+#include <errno.h>
 
 int main()
 {
 	// char *user = getenv("USER");
 	// char *machine = getenv("MACHINE");
-	// char *pwd = getenv("PWD"); //parent working directory
+	// char *pwd = getenv("PWD"); //print working directory
 
 	while (1) {
 		printf("%s@%s:%s>", getenv("USER"), getenv("MACHINE"), getenv("PWD"));
@@ -137,24 +140,33 @@ void doCode(tokenlist *tokens){
 		}
 	} //end of echo
 	//ls
-	else if(!(strcmp(first, "ls"))){
-		//**The following code does not work but it does delimit $PATH correctly
+	else if((strcmp(first, "ls")) == 0){
+		// //**The following code does not work but it does delimit $PATH correctly
 		// //get directory by env and then get entities 
 		// if(tokens->size == 1){ //nothing after ls
 		// 	printf("Inside ls \n");
 		// 	//I think we need to use strtok() for this
 		// 	// char * path = getenv("PATH");
-		// 	char * list = strtok(getenv("PATH"), ":");
+		// 	char * list = strtok(getenv("$PATH"), ":");
 		// 	while(list != NULL){
+		// 		printf("Inside while\n");
 		// 		printf("%s ", list);
 		// 		list = strtok(NULL, ":");
 		// 	}
 		// }
 		// printf("done ls code\n");
+		char * cmd[] = {"ls",  NULL};
+		int pid = fork();
+		if(pid == 0){
+			printf("before\n");
+			execv("ls", cmd);
+			printf("After\n");
+		}
 	}
 	else if((strcmp(first, "cd")) == 0){
 		if((tokens->size == 1) || (strcmp(tokens->items[1], "~") == 0)){ //nothing after cd, or ~ after, so make PWD, HOME
 			setenv("PWD", getenv("$HOME"), 1); //1 means if PWD exists, it is updated
+			chdir(getenv("$HOME"));
 			printf("done\n");
 		}
 		else if(strcmp(tokens->items[1], "..") == 0){ //cd ..(go back one directory)
@@ -164,9 +176,8 @@ void doCode(tokenlist *tokens){
 				//might want to change above function to strtok() because that is what he uses in the shell PP
 				if (last_slash != NULL) { //need to have found a "/"
 					*last_slash = '\0'; //null-terminate the string at the last slash
-					setenv("PWD", current_directory, 1); //change directory
+					setenv("PWD", current_directory, 1); //change print working directory
 					chdir(current_directory);
-					printf("Simulating a change to the parent directory: %s\n", current_directory);
 				} else {
 					printf("Already at the root directory; cannot go up.\n");
 				}
@@ -176,6 +187,25 @@ void doCode(tokenlist *tokens){
 			}
 		}
 		//need to add if cd into folder into folder 
+		else if (strcmp(tokens->items[1], "./bin") == 0){ //if cd ~
+			//cd ~(something after)
+			//check if file directory exists before going into it 
+			// . is current directory
+			// ~ is home directory
+			printf("got in cd .\n");
+			DIR* dir = opendir("bin");
+			printf("got here\n");
+			if (dir) {
+				/* Directory exists. */
+				setenv("PWD", tokens->items[1], 1); //change print working directory
+				chdir(tokens->items[1]);
+				closedir(dir);
+			} else if (ENOENT == errno) {
+				/* Directory does not exist. */
+			} else {
+				/* opendir() failed for some other reason. */
+			}
+		}
 	}
 
 }
@@ -202,17 +232,12 @@ char* tilde(const char* token)
 void tilde(char *token){
 	if(token[0] == '~'){
 			char* replace = token; //get the token that has ~
-			printf("line 97\n");
 			replace++; //get rid of ~
-			printf("line 99\n");
 			char* adding = "$HOME";
-			printf("line 101\n");
 			char expanded[strlen(adding) + strlen(token)];
-			printf("line 104\n");
 			sprintf(expanded, "%s%s", adding, token+1);
-			printf("line 106\n");
 			strcpy(token, expanded);
-			printf("line 108\ntok is %s\n", token);
+			printf("tok is %s\n", token);
 		}
 }
 
